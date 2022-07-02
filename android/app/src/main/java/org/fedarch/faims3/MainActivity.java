2dd9bd720c855e7140da5779939ff056ae983002
Steve Cassidy <steve.cassidy@mq.edu.au>
2022-07-03 09:51:09 +1000

    add android project

---
+++ android/app/src/main/java/org/fedarch/faims3/MainActivity.java
@@ -0,0 +1,5 @@
+package org.fedarch.faims3;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'org.fedarch.faims3',
+  appName: 'faims3-qrcode-example',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
