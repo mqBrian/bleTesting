863f93d05d69c3c309d1d9007875fbabcb081629
mqBrian <brian-david-mbalala.wekulo@students.mq.edu.au>
2022-09-12 13:35:52 +1000

    Recent Push

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
 //@ts-ignore
-import React from 'react'
-import './App.css'
-import './BleTest';
-import { scan } from './BleTest';
+import React from "react";
+import "./App.css";
+import "./BleTest";
+import { scan } from "./BleTest";
 
-function clickMe (){
-  alert('You clicked me!');
+function clickMe() {
+  alert("You clicked me!");
 }
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <h1>This is a simple BT app</h1>
-      <h2>What are you looking at you frog</h2>
-      <button onClick = {() => scan}>
-        Button
-      </button>
+      <button onClick={scan}>Button</button>
+      <button onClick={() => console.log("Button was clicked")}>Alert</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
