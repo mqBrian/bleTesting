121e29bc85f61affc2b1bd21d0d61d811bbf9368
mqBrian <brian-david-mbalala.wekulo@students.mq.edu.au>
2022-09-13 15:11:28 +1000

    Fixed Android
    I worked with Steve and he helped me to get the project working on Android

--- src/BleTest.tsx
+++ src/BleTest.tsx
@@ -1,4 +1,4 @@
-import { BleClient, numberToUUID } from '@capacitor-community/bluetooth-le';
+import { BleClient, numberToUUID } from "@capacitor-community/bluetooth-le";
 
 const HEART_RATE_SERVICE = numberToUUID(0x180d);
 
@@ -8,16 +8,16 @@ export async function scan(): Promise<void> {
 
     await BleClient.requestLEScan(
       {
-        services: [HEART_RATE_SERVICE],
+        // services: [HEART_RATE_SERVICE],
       },
       (result) => {
-        console.log('received new scan result', result);
+        console.log("received new scan result", JSON.stringify(result));
       }
     );
 
     setTimeout(async () => {
       await BleClient.stopLEScan();
-      console.log('stopped scanning');
+      console.log("stopped scanning");
     }, 5000);
   } catch (error) {
     console.error(error);
